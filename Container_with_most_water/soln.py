8ee7b707458796e1bbe44b3aec8bb07761c6a8c5
Nnamdi Ajoku <90708381+ajaycmput@users.noreply.github.com>
2022-07-10 13:40:50 -0600

    solved-very fast

--- Container_with_most_water/soln.py
+++ Container_with_most_water/soln.py
@@ -2,14 +2,14 @@
     Name: Nnamdi Ajoku
 
     Language: Python
-    
+
     Description: You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
 
     Find two lines that together with the x-axis form a container, such that the container contains the most water.
 
     Return the maximum amount of water a container can store.
 """
-def maxArea(self, height: List[int]) -> int:
+def maxArea(height) -> int:
     max = 0
     lr = 0
     rp = len(height) - 1
