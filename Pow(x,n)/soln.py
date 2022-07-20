e6ed23513e7f6ac2f559655f1b4a4d55b6c4f138
Nnamdi Ajoku <90708381+ajaycmput@users.noreply.github.com>
2022-07-20 06:52:41 -0600

    Works, will IMPLEMENT from scratch soon

---
+++ Pow(x,n)/soln.py
@@ -0,0 +1,9 @@
+'''
+    Name: Nnamdi Ajoku
+    Language: Python
+    
+    Implement pow(x, n), which
+    calculates x raised to the power n (i.e., xn).
+'''
+def pow_mod(x,n):
+    return x ** n
